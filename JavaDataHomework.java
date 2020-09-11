b6eb8e2b46e4dffea04606dbb1603bd71610d768
unknown <nigelmstr@gmail.com>
2020-09-11 12:31:21 -0400

    adding JavaDataHomework

---
+++ JavaDataHomework.java
@@ -0,0 +1,28 @@
+public class JavaDataHomework {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String name = "Nigel"; 
+		// create a variable that holds a name
+		
+		System.out.println(name);
+		//output the name to the console
+		
+		int age = 10;
+		//create a variable that holds an age
+		
+		System.out.println(age);
+		//output the age to the console
+		
+		double decimal = 1.3;
+		//create a variable that holds a decimal
+		
+		System.out.println(decimal);
+		//output the variable
+		
+		
+
+	}
+
+}
