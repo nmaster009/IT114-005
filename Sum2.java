1b851fb6c66d51a33095100fc05447da2ff6279b
unknown <nigelmstr@gmail.com>
2020-09-29 20:59:07 -0400

    This is my answer for the recursion hw

---
+++ Sum2.java
@@ -0,0 +1,15 @@
+public class Sum2 {
+
+    public static void main(String[] args) {
+
+    	  int num = 9, sum = 3;  //this will loop from 1 to 9 and it will also add all numbers sum
+
+          for(int i = 1; i <= num; i++)  //
+          {
+               sum = sum + i;
+              
+          }
+
+          System.out.println("THe Sum of these numbers will be = " + sum);
+      }
+  }
