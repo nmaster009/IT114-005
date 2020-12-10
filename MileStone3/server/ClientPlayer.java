7e192b0d87106c0207db8cbe118c7ca14daa6241
unknown <nigelmstr@gmail.com>
2020-12-09 23:00:45 -0500

    updated files for milestone3

---
+++ MileStone3/server/ClientPlayer.java
@@ -0,0 +1,14 @@
+  
+package server;
+
+//import client.Player;
+
+public class ClientPlayer {
+    public ClientPlayer(ServerThread client) {
+	this.client = client;
+//	this.player = player;
+    }
+
+    public ServerThread client;
+   // public Player player;
+}
